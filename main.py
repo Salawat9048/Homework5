f1d9428c15be1a3ea637efe1b9488200abd2ec81
Salawat9048 <salawat9048@outlook.com>
2024-06-01 20:17:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+immutable_war =1, 2, 'salawat', [1, 2, 3]
+print(immutable_war)
+mutable_list =([1, 2, 3, 4,], 'str',)
+print(mutable_list)
+mutable_list[0][3] =5
+print(mutable_list)
+# print
+immutable_war =1, 2, 'salawat', [1, 2, 3,]
+immutable_war[0] =5
+print(immutable_war)
